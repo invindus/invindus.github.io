7158e14af62fd50ee5b193ddf142b1c6f54a0a6d
invindus <snchanXV@gmail.com>
2024-05-10 16:07:52 -0600

    Title Page * Title & social media icons * Move icons below "I'm Sam" if mobile, to the right if large screen

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,66 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useState, useEffect } from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  
+  // [CHECK MOBILE] ------------------------------------------------------------------------
+  const [isMobile, setIsMobile] = useState(false);
+  const checkMobile = () => {
+    setIsMobile(window.innerWidth <= 600); // Adjust the threshold as needed
+  };
+
+  useEffect(() => {
+    checkMobile();
+    window.addEventListener('resize', checkMobile);
+    return () => {
+      window.removeEventListener('resize', checkMobile);
+    };
+  }, []); // Empty dependency array ensures this effect runs only once when component mounts
+
+  // END [CHECK MOBILE] ---------------------------------------------------------------------
+
+
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className='page-1'>
+
+        <div className='title'>
+          <h1>Hey,</h1> 
+
+          <div className='title-icon'> 
+            <h1>I'm Sam</h1>
+
+            {/* Show icons to the right of I'm Sam if large screen */}
+            {!isMobile && (
+              <div className='profile-icons'>
+                <button className='gh-icon'> </button>
+                <button className='ig-icon'> </button>
+                <button className='ln-icon'> </button>
+              </div>
+            )}
+            
+          </div>
+
+        </div>
+
+        {/* Show icons below I'm Sam if mobile screen */}
+        {isMobile && (
+          <div className='profile-icons'>
+            <button className='gh-icon'> </button>
+            <button className='ig-icon'> </button>
+            <button className='ln-icon'> </button>
+          </div>
+        )}
+
+        
+
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+
+      <div className='page-2'>
+        <h2>In progress...</h2>
+
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
