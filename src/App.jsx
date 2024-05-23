4b5983ba767161a0f50c79234d150a70c3fa5557
Sam Chan <Invindus@hotmail.com>
2024-05-23 14:13:51 -0600

    icon change, profile description

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ function App() {
   // [CHECK MOBILE] ------------------------------------------------------------------------
   const [isMobile, setIsMobile] = useState(false);
   const checkMobile = () => {
-    setIsMobile(window.innerWidth <= 600); // Adjust the threshold as needed
+    setIsMobile(window.innerWidth <= 700); // Adjust the threshold as needed
   };
 
   useEffect(() => {
@@ -26,36 +26,46 @@ function App() {
       <div className='page-1'>
 
         <div className='title'>
-          <h1>Hey,</h1> 
-
-          <div className='title-icon'> 
-            <h1>I'm Sam</h1>
-
-            {/* Show icons to the right of I'm Sam if large screen */}
-            {!isMobile && (
+          <h1>Hey, <br/>I'm Sam</h1> 
+            
+          {/* Show icons to the right of I'm Sam if large screen */}
+          {!isMobile && (
+            <div className='profile'> 
               <div className='profile-icons'>
                 <button className='gh-icon'> </button>
                 <button className='ig-icon'> </button>
                 <button className='ln-icon'> </button>
               </div>
-            )}
-            
-          </div>
+
+              <div className='profile-desc'> 
+                <h2> - student, developer,</h2>
+                <h2> photographer.</h2>
+              </div>
+            </div>
+          )}
 
         </div>
 
         {/* Show icons below I'm Sam if mobile screen */}
         {isMobile && (
-          <div className='profile-icons'>
-            <button className='gh-icon'> </button>
-            <button className='ig-icon'> </button>
-            <button className='ln-icon'> </button>
+          <div className='profile-mobile'>
+            <div className='profile-icons-mobile'>
+              <button className='gh-icon'> </button>
+              <button className='ig-icon'> </button>
+              <button className='ln-icon'> </button>
+            </div>
+            
+            <div className='profile-desc'> 
+              <h2> - student, developer,</h2>
+              <h2> photographer.</h2>
+            </div>
           </div>
         )}
 
         
 
       </div>
+      {/* END PAGE 1 */}
 
       <div className='page-2'>
         <h2>In progress...</h2>
