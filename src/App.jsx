35f0e023458183c2e9b387d9e3690778dd790b33
Sam Chan <Invindus@hotmail.com>
2025-06-12 21:52:41 -0600

    Restarted project. - Added landing page - Added home page - Created empty about, dev, and photos pages.

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { useState, useEffect } from 'react'
 import './App.css'
+import Home from './pages/Home/Home';
 
 function App() {
   
@@ -23,54 +24,15 @@ function App() {
 
   return (
     <>
-      <div className='page-1'>
+      <Home/>
 
-        <div className='title'>
-          <h1>Hey, <br/>I'm Sam</h1> 
-            
-          {/* Show icons to the right of I'm Sam if large screen */}
-          {!isMobile && (
-            <div className='profile'> 
-              <div className='profile-icons'>
-                <button className='gh-icon'> </button>
-                <button className='ig-icon'> </button>
-                <button className='ln-icon'> </button>
-              </div>
-
-              <div className='profile-desc'> 
-                <h2> - student, developer,</h2>
-                <h2> photographer.</h2>
-              </div>
-            </div>
-          )}
-
-        </div>
-
-        {/* Show icons below I'm Sam if mobile screen */}
-        {isMobile && (
-          <div className='profile-mobile'>
-            <div className='profile-icons-mobile'>
-              <button className='gh-icon'> </button>
-              <button className='ig-icon'> </button>
-              <button className='ln-icon'> </button>
-            </div>
-            
-            <div className='profile-desc'> 
-              <h2> - student, developer,</h2>
-              <h2> photographer.</h2>
-            </div>
-          </div>
-        )}
-
-        
-
-      </div>
-      {/* END PAGE 1 */}
-
-      <div className='page-2'>
+      <div className='in-progress'>
         <h2>In progress...</h2>
 
       </div>
+      <footer class="footer">
+        © {new Date().getFullYear()} Samuel Chan
+      </footer>
     </>
   )
 }
---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,53 @@
+import React from 'react'
+import './Home.css';
+
+const Home = () => {
+  return (
+    <>
+      <div className='intro'>
+
+        <div className='title'>
+          <h1>SAMUEL CHAN</h1>
+          <div className='subtitles'>
+            <p>student,</p>
+            <p>developer,</p>
+            <p>photographer</p>
+          </div>
+        </div>
+            
+
+
+
+      </div>
+      {/* END PAGE 1 */}
+
+      <div className='home'>
+        <div className='border'>
+
+        
+          <h1>SAMUEL CHAN</h1>
+          <div className='nav'>
+            <p>development</p>
+            <p>about</p>
+            <p>photography</p>
+          </div>
+
+          <div className='profiles'>
+            <a href="https://github.com/invindus" target='_blank' rel='noopener noreferrer' class='gh-link'>
+              <img src="/github.svg" alt="GitHub"/>
+            </a>
+            <a href="https://www.instagram.com/inv.indus/" target='_blank' rel='noopener noreferrer' class='ig-link'>
+              <img src="/instagram.svg" alt="Instagram"/>
+            </a>
+            <a href="https://www.linkedin.com/in/snchanxv/" target='_blank' rel='noopener noreferrer' class='ln-link'>
+              <img src="/linkedin.svg" alt="LinkedIn"/>
+            </a>
+          </div>
+
+        </div>
+      </div>
+  </>
+  )
+}
+
+export default Home
